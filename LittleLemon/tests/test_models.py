772c7bddeba4cafc979f9f3ad87b305e49bf8d8c
RyW90 <42977641+RyW90@users.noreply.github.com>
2024-01-09 21:55:28 +0800

    Update test_models.py

--- LittleLemon/tests/test_models.py
+++ LittleLemon/tests/test_models.py
@@ -7,7 +7,6 @@ class MenuTestCase(TestCase):
         Menu.objects.create(title="Test Menu Item", price=10.00, inventory=100)
 
     def test_menu_item(self):
-        """Menu item should have a title, price and inventory"""
         menu_item = Menu.objects.get(title="Test Menu Item")
         self.assertEqual(menu_item.title, "Test Menu Item")
         self.assertEqual(menu_item.price, 10.00)
